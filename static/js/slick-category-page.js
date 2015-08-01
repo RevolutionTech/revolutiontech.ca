383cb2feefb2aa5ba8e041e299e579b47d51beb4
Lucas Connors <lucas.revolutiontech@gmail.com>
2015-08-01 16:27:11 -0700

    Make category page slider more responsive on mobile using slick.js

---
+++ static/js/slick-category-page.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.hero-units').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: "<button type=\"button\" class=\"slick-prev show-for-large-up\">Previous</button>",
+        nextArrow: "<button type=\"button\" class=\"slick-next show-for-large-up\">Next</button>",
+    });
+});
